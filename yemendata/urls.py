015ee5171d6286fbf139c8cfb189443e5bf07045
Hicham <ezzine@hotmail.com>
2012-04-18 14:25:47 +0000

    Use the standalone staticfiles instead of the one built in django

--- yemendata/urls.py
+++ yemendata/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import *
 from django.conf import settings
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
 from django.views.generic.simple import direct_to_template
 
